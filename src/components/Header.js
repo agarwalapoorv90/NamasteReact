6bab08c9cdf82a05d829d1fb60eb8daf44a73d62
Apoorv Agarwal <chelstar90@Apoorvs-MacBook-Air.local>
2023-02-02 14:13:24 +0530

    chapter 7

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,25 @@
 import { useState } from "react";
+import Logo from "../assets/img/logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className="header">
       <div className="title">
-        <a href="/">
-          <img
-            className="logo"
-            src="https://images.hindustantimes.com/img/2022/09/22/1600x900/Swiggy_1663839495104_1663839495282_1663839495282.jpg"
-            alt="Food Logo"
-          />
-        </a>
+        <Link to="/">
+          <img className="logo" src={Logo} alt="Food Logo" />
+        </Link>
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
           <li>Categories</li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
           <li>Cart</li>
         </ul>
         {!loggedIn ? (
